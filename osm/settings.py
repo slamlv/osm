055f438e40f5c77716aa5ef3a628037481ee2f12
slamlv <77462962+slamlv@users.noreply.github.com>
2025-11-20 03:33:25 +0100

    Update settings.py

--- osm/settings.py
+++ osm/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Fichiers
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media'
+MEDIA_ROOT = '/app/medi'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL)
 ]
