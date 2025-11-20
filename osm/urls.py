2f6fcc5a8810cb4ec14cba789dc76dd3e586dc7b
slamlv <77462962+slamlv@users.noreply.github.com>
2025-11-20 05:19:41 +0100

    Update urls.py

--- osm/urls.py
+++ osm/urls.py
@@ -38,4 +38,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     # Production
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
