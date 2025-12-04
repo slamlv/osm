8b0db12290bbd3acf3937f959c2b3ec4d108d490
slamlv <77462962+slamlv@users.noreply.github.com>
2025-12-04 01:05:37 +0100

    Update utils.py

--- osm/utils.py
+++ osm/utils.py
@@ -180,7 +180,7 @@ class BaseListView(View):
         return datas
 
 
-class ListView(AdminRequired, WithUsersSchoolSchema, BaseListView):
+class ListView(LoginRequired, AdminRequired, WithUsersSchoolSchema, BaseListView):
     pass
 
 
@@ -499,7 +499,7 @@ def cote_and_appr(note):
     return cote, appr
 
 
-def resize_image(image_path, new_width=295, quality=85, return_height=False, id_card=False):
+def resize_image(image_path, new_width=295, quality=100, return_height=False, id_card=False):
     img = Image.open(image_path)
     if id_card:
         new_size = (413, 472)
