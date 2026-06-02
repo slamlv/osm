e911fc55538948df8087273c8c791f34c344201b
slamlv <monteilalpha1@gmail.com>
2026-06-02 05:08:14 +0100

    Updates

--- osm/urls.py
+++ osm/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
+
 from .views import index, ajax_messages, SchoolDetails, TranchesHorairesEdit, SchoolInformations, offline_index
 
 urlpatterns = [
@@ -33,4 +34,5 @@ urlpatterns = [
     path("", include("classroom.urls")),
     path("", include("student.urls")),
     path("", include("note.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+
