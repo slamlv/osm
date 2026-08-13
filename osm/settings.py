9261af8c345e1f3f4f1e0db9b28ae8cdc3876970
slamlv <monteilalpha1@gmail.com>
2026-08-13 04:32:47 +0100

    Updates

--- osm/settings.py
+++ osm/settings.py
@@ -111,8 +111,14 @@ DATABASE_ROUTERS = (
     'django_tenants.routers.TenantSyncRouter',
 )
 
+DATABASE_URL = config('DATABASE_URL')
+
 DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+    "default": dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 DATABASES['default']['ENGINE'] = 'django_tenants.postgresql_backend'
